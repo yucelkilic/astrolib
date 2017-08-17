7cc1d5db468e0031ef0f62260eb189f6d1b3f726
Yücel KILIÇ <yucelkilic@myrafproject.org>
2017-08-17 15:50:09 +0300

    add plot asteroids even don't have a fits image

--- visuals.py
+++ visuals.py
@@ -10,6 +10,8 @@ from astropy.io import fits
 from astropy.table import Table
 from astropy import coordinates
 from astropy import units as u
+from astroquery.skyview import SkyView
+import astropy.units as u
 
 import numpy as np
 import sep
@@ -61,21 +63,36 @@ class StarPlot:
 
         return(True)
 
-    def asteroids_plot(self, image_path, time_travel=1, max_mag=20.0):
+    def asteroids_plot(self, image_path=None,
+                       ra=None,
+                       dec=None,
+                       odate=None,
+                       time_travel=1,
+                       radius=6,
+                       max_mag=20.0,
+                       circle_color='yellow',
+                       arrow_color='red'):
 
         """
         Source plot module.
-        @param image_data: data part of the FITS image
-        @type image_data: numpy array
-        @param objects: Return of the detect_sources
-        function with skycoords.
-        @type objects: astropy.table
+        @param image_path: data part of the FITS image
+        @type image_path: numpy array
+        @param ra: RA coordinate of target area.
+        @type ra: str in "HH MM SS"
+        @param dec: DEC coordinate of target area
+        @type dec: str in "+DD MM SS"
+        @param radius: Radius in arcmin.
+        @type radius: str
+        @param odate: Ephemeris date of observation in date
+        @type odate: "2017-08-15T19:50:00.95" format in str
         @param time_travel: Jump into time after given date (in hour).
         @type time_travel: float
         @param max_mag: Limit magnitude to be queried object(s)
         @type max_mag: float
-        @param mark_color: Color of the plot marks
-        @type mark_color: str
+        @param circle_color: Color of the asteroids marks
+        @type circle_color: str
+        @param arrow_color: Color of the asteroids direction marks
+        @type arrow_color: str
         @returns: boolean
         """
 
@@ -83,7 +100,24 @@ class StarPlot:
         rcParams['figure.figsize'] = [10., 8.]
         # rcParams.update({'font.size': 10})
 
-        hdu = fits.open(image_path)[0]
+        if image_path:
+            hdu = fits.open(image_path)[0]
+        elif not image_path and ra and dec and odate:
+            co = coordinates.SkyCoord('{0} {1}'.format(ra, dec),
+                                      unit=(u.hourangle, u.deg),
+                                      frame='icrs')
+            print('Target Coordinates:',
+                  co.to_string(style='hmsdms', sep=':'),
+                  'in {0} arcmin'.format(radius))
+            try:
+                server_img = SkyView.get_images(position=co,
+                                                survey=['DSS'],
+                                                radius=radius*u.arcmin)
+                hdu = server_img[0][0]
+            except:
+                print("SkyView could not get the image from DSS server.")
+                raise SystemExit
+
         wcs = WCS(hdu.header)
 
         data = hdu.data.astype(float)
@@ -107,16 +141,19 @@ class StarPlot:
         overlay[0].set_axislabel('Right Ascension (ICRS)')
         overlay[1].set_axislabel('Declination (ICRS)')
 
-        fo = FitsOps(image_path)
-        odate = fo.get_header('date-obs')
-        
         ac = AstCalc()
-        ra_dec = ac.center_finder(image_path, wcs_ref=True)
+        if image_path:
+            fo = FitsOps(image_path)
+            odate = fo.get_header('date-obs')
+            ra_dec = ac.center_finder(image_path, wcs_ref=True)
+        elif not image_path and ra and dec and odate:
+            odate = odate
+            ra_dec = [co.ra, co.dec]
 
         asteroids = Table(ac.find_skybot_objects(odate,
                                                  ra_dec[0].degree,
                                                  ra_dec[1].degree,
-                                                 radius=8),
+                                                 radius=radius),
                           names=('num',
                                  'name',
                                  'ra(h)',
@@ -130,7 +167,7 @@ class StarPlot:
             odate,
             ra_dec[0].degree,
             ra_dec[1].degree,
-            radius=8,
+            radius=radius,
             time_travel=time_travel),
                                 names=('num',
                                        'name',
@@ -163,7 +200,7 @@ class StarPlot:
                     transform=ax.get_transform('icrs'))
 
                 p = Circle((c.ra.degree, c.dec.degree), 0.005,
-                           edgecolor='yellow',
+                           edgecolor=circle_color,
                            facecolor='none',
                            transform=ax.get_transform('icrs'))
                 ax.text(c.ra.degree,
@@ -176,7 +213,7 @@ class StarPlot:
                         transform=ax.get_transform('icrs'))
                 
                 r.set_facecolor('none')
-                r.set_edgecolor('red')
+                r.set_edgecolor(arrow_color)
                 ax.add_patch(p)
                 ax.add_patch(r)
         # plt.gca().invert_xaxis()
